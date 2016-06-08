e933a4f2117fd10e25afc3dcd625d7a3ba217766
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-06-08 12:30:01 -0700

    Sinon setup (hopefully) complete.

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,8 @@
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  // before and after using sinon here
+
   it('todo.setup receives an array of todos when todo.init is called', function () {
   });
 });
